f445c0f0c07a2c2db3612797c59576b66432fb83
Kristof Vannotten <kristof@vannotten.be>
2013-07-24 21:11:59 +0800

    Added connection

--- lib/mustached_octo_wookie.rb
+++ lib/mustached_octo_wookie.rb
@@ -2,5 +2,12 @@ require "mustached_octo_wookie/version"
 require "mustached_octo_wookie/irc"
 
 module MustachedOctoWookie
-
+  attr_reader :connection
+  
+  def self.connect server, port, nick
+    @connection = MustachedOctoWookie::IRC.new server, port, nick
+    @connection.connect
+    
+    @connection
+  end
 end
